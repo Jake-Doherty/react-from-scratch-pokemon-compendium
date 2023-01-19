14b05389303a3a89698d0faecd34124a0869a99e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-19 15:22:10 -0800

    creates select and query components then mount to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { usePokemon } from '../hooks/usePokemon.js';
+import Select from './Select/Select.js';
 
 export default function Main() {
   const pokemon = usePokemon();
+  console.log('pokemon', pokemon);
   return (
     <div>
+      <Select />
       {pokemon.map((monster) => (
         <p key={monster.id}>{monster.pokemon}</p>
       ))}
