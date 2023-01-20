deb293d20c67eb63419144f5de0d1f573dcaa15e
Dylan <dylflanagan@gmail.com>
2023-01-19 16:33:08 -0800

    HP added as third attribute to PokemonCard.js

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default function PokemonCard({ pokemon, url_image, pokedex }) {
-  console.log(url_image, pokedex);
+export default function PokemonCard({ pokemon, url_image, pokedex, hp }) {
+  // console.log(url_image, pokedex);
   return (
     <div>
       <h2>{pokemon}</h2>
       <a href={pokedex}>
-        {' '}
         <img src={url_image} />
       </a>
+      <h4>HP: {hp}</h4>
     </div>
   );
 }
