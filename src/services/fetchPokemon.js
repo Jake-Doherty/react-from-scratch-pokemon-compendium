75a51e49943a49b116e42b170a50a9102c70fcad
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-19 16:08:24 -0800

    adds types fetch

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -1,5 +1,13 @@
 export async function fetchInitialPokemon() {
   const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/');
+
   const data = await resp.json();
+
   return data.results;
+}
+
+export async function fetchPokemonType() {
+  const resp = await fetch('https://alchemy-pokedex.herokuapp.com/api/pokedex/types');
+  const data = await resp.json();
+  return data;
 }
