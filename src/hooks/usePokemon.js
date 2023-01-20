ed3d78967e7b0909e754df08b3030b147fa8710b
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-19 16:09:18 -0800

    import types fetch and add useEffect to get data

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
-import { fetchInitialPokemon } from '../services/fetchPokemon.js';
+import { fetchInitialPokemon, fetchPokemonType } from '../services/fetchPokemon.js';
 
 export function usePokemon() {
   const [pokemon, setPokemon] = useState([]);
+  const [types, setTypes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,5 +14,16 @@ export function usePokemon() {
     };
     fetchData();
   }, []);
-  return pokemon;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // load types
+      const data = await fetchPokemonType();
+      // set types in state
+      setTypes(data);
+    };
+    fetchData();
+  }, []);
+
+  return { pokemon, types };
 }
