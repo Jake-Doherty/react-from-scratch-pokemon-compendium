46f162eaad3ba45afc4da32b942cb23910f1c97d
Dylan <dylflanagan@gmail.com>
2023-01-19 16:29:39 -0800

    Pokemon image displaying - Pokedex attribute linked as href

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export default function PokemonCard({ pokemon }) {
+export default function PokemonCard({ pokemon, url_image, pokedex }) {
+  console.log(url_image, pokedex);
   return (
     <div>
       <h2>{pokemon}</h2>
+      <a href={pokedex}>
+        {' '}
+        <img src={url_image} />
+      </a>
     </div>
   );
 }
