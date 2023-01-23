e96b08b2e31f9143745aa464d2396170424bec9a
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-22 23:55:11 -0800

    adds background asset and styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import './App.css';
 import Main from './components/Main/Main.js';
 import Header from './components/Header/Header.js';
 import Footer from './components/Footer/Footer.js';
+import background from './pokemon-grass.jpg';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: 'repeat',
+        backgroundPosition: 'center',
+      }}
+    >
       <Header />
       <Main />
       <Footer />
